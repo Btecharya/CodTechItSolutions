a5a7b91690a33c5cfdb39d18ceb809b054ce0dc2
Ashish Sahani <90511449+Btecharya@users.noreply.github.com>
2024-03-15 11:43:15 +0530

    Add files via upload

---
+++ 2.SimpleCalculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Error! Division by zero is not allowed." << endl;
+            break;
+        default:
+            cout << "Invalid operation! Please enter one of the following: +, -, *, /" << endl;
+    }
+
+    return 0;
+}
